834fc33d99acead06a0011d745067b2ca5c773a3
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2026-01-10 09:52:39 -0300

    feat: create migration adding user role to user table

---
+++ database/migrations/2026_01_10_124222_add_role_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Enums\PaymentStatus;
+use App\Enums\UserRoles;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', UserRoles::cases())->default(UserRoles::USER);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
