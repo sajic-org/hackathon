4bc78254b6fd74e7475475729d7e97db5918c930
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2026-01-10 09:31:49 -0300

    feat: create sponsor leads table and model

---
+++ app/Models/SponsorLead.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SponsorLead extends Model
+{
+    /** @use HasFactory<\Database\Factories\SponsorLeadFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'message',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at'=> 'datetime',
+    ];
+}
