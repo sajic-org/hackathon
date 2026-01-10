bb18dcd9b6da030f197b6dce96fd36756f7d317e
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2026-01-10 09:32:08 -0300

    feat: create sponsor leads factory and seeder

---
+++ database/seeders/SponsorLeadSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SponsorLead;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SponsorLeadSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        SponsorLead::factory(50)->create();
+    }
+}
